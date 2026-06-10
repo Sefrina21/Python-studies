e298f364de8619eec703961d780a68445f9e6537
Sefrina21 <sefrinawinfreda@karunya.edu.in>
2026-06-10 19:23:19 +0530

    Create Exercise 5.py

---
+++ Day-3/Exercise 5.py
@@ -0,0 +1,12 @@
+employee = {
+    "employee_name": "Rahul",
+    "age": 28,
+    "company": "Tech Solutions",
+    "department": "Web Development",
+    "joining_date": "15-06-2024",
+    "employee_id": 102,
+    "manager": "Mr. Kumar"
+}
+
+for key, value in employee.items():
+    print(key, ":", value)
